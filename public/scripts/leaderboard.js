b3cfd2b3d20ffc809edcf36d5860387ed1e5026f
TheModCraft <128427785+GruenerNinja@users.noreply.github.com>
2025-03-14 13:55:44 +0100

    + Leaderboard Done

--- public/scripts/leaderboard.js
+++ public/scripts/leaderboard.js
@@ -5,23 +5,25 @@ document.addEventListener('DOMContentLoaded', async () => {
         const response = await fetch('http://172.16.2.165:3000/users');
         const data = await response.json();
 
-
         // Add each user as a row in the table
         data.forEach((user, i) => {
             const row = document.createElement('tr');
-            if (i < 4 ) {
+            row.addEventListener('click', function() {
+                window.location.href = `/profile-page.html?username=${user.username}`;
+            });
+            if (i < 3) {
                 row.innerHTML = `
-                    <i class="fa-solid fa-medal"></i>
+                    <i class="fa-solid fa-trophy" style="color:var(${"--"+(i+1)})"></i>
                     <td>${"#" + (i + 1)}</td>  
                     <td>${user.username}</td>
-                    <td>${user.score}</td>
+                    <td>${user.topscore}</td>
                 `;
             }else {
                 row.innerHTML = `
                     <td></td>
                     <td>${"#" + (i + 1)}</td>  
                     <td>${user.username}</td>
-                    <td>${user.score}</td>
+                    <td>${user.topscore}</td>
                 `;
             }
             table.appendChild(row);
