6928f438c39c88d9ce9b68f2416598328a14506e
Yanik Flückiger <yanik.flueckiger@ict-campus.net>
2025-03-14 16:36:35 +0100

    removed lines like a god

--- public/scripts/OTetromino.js
+++ public/scripts/OTetromino.js
@@ -43,24 +43,4 @@ export class OTetromino extends Tetromino{
     rotate() {
         //be sad
     }
-
-    //translates my cords in the grid cords
-    getGridPosition() {
-        return [
-            [this.position[0][0] + this.shiftX, this.position[0][1] + this.shiftY],
-            [this.position[1][0] + this.shiftX, this.position[1][1] + this.shiftY],
-            [this.position[2][0] + this.shiftX, this.position[2][1] + this.shiftY],
-            [this.position[3][0] + this.shiftX, this.position[3][1] + this.shiftY]
-        ];
-    }
-
-    //translates the grid cords in element id
-    getElementIdGrid (gridPosition) {
-        return [
-            String(gridPosition[0][0]) + String(gridPosition[0][1]),
-            String(gridPosition[1][0]) + String(gridPosition[1][1]),
-            String(gridPosition[2][0]) + String(gridPosition[2][1]),
-            String(gridPosition[3][0]) + String(gridPosition[3][1])
-        ];
-    }
 }
--- public/scripts/STetromino.js
+++ public/scripts/STetromino.js
@@ -74,28 +74,4 @@ export class STetromino extends Tetromino {
             this.setPosition(this.pos1);
         }
     }
-
-    //translates my cords to grid cords
-    getGridPosition() {
-        return [
-            [this.position[0][0] + this.shiftX, this.position[0][1] + this.shiftY],
-            [this.position[1][0] + this.shiftX, this.position[1][1] + this.shiftY],
-            [this.position[2][0] + this.shiftX, this.position[2][1] + this.shiftY],
-            [this.position[3][0] + this.shiftX, this.position[3][1] + this.shiftY]
-        ];
-    }
-
-    //translates grid cords to element id
-    getElementIdGrid (gridPosition) {
-        return [
-            String(gridPosition[0][0]) + String(gridPosition[0][1]),
-            String(gridPosition[1][0]) + String(gridPosition[1][1]),
-            String(gridPosition[2][0]) + String(gridPosition[2][1]),
-            String(gridPosition[3][0]) + String(gridPosition[3][1])
-        ];
-    }
-
-    setPosition(pos) {
-        this.position = pos;
-    }
 }
